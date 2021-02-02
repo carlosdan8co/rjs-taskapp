0e38378fde58f4512c6594643b0e48643901bcaa
Dan <yosoy@carlosdan8.co>
2021-02-02 16:17:54 -0600

    First commint

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React,{useState,useEffect} from 'react'
+import {TaskRow} from './components/TaskRow'
+import {TaskBanner} from "./components/TaskBanner";
+import {TaskCreator} from "./components/TaskCreator";
+import {VisibilityControl} from './components/VisibilityControl'
+
+function App() {
+
+  const [userName, setUserName] = useState('Phoemx')
+  const [taskItems, setTaskItems] = useState([
+    {name:'Task One',done:false},
+    {name:'Task Two',done:false},
+    {name:'Task Three',done:false},
+    {name:'Task Four',done:true},
+  ])
+
+  const [showCompleted,setShowCompleted] = useState(true)
+
+  useEffect(()=>{
+    let data= localStorage.getItem('tasks')
+    if (data!=null){
+      setTaskItems(JSON.parse(data))
+    }else{
+      setUserName('fazt ex')
+      setTaskItems([
+        {name:'Task One ex',done:false},
+        {name:'Task Two ex',done:false},
+        {name:'Task Three ex',done:false},
+        {name:'Task Four ex',done:true}
+      ])
+      setShowCompleted(true)
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem('tasks',JSON.stringify(taskItems))
+  },[taskItems])
+
+  const createNewTask = taskName =>{
+    if(!taskItems.find(t=>t.name===taskName)){
+      setTaskItems([...taskItems,{name:taskName,done:false}])
+    }
+  }
+
+  const toggleTask =task =>
+    setTaskItems(taskItems.map(t=>(t.name === task.name?{...t,done:!t.done}:t)))
+
+  const taskTableRows = doneValue =>
+    taskItems
+    .filter(task=>task.done===doneValue)
+    .map(task=>(
+      <TaskRow task={task} key={task.name} toggleTask={toggleTask}/>
+    ))
+  
+
+  return (
+    <div >
+      <TaskBanner userName={userName} taskItems={taskItems}/>
+      <TaskCreator callback={createNewTask}/>
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr >
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskTableRows(false)}
+        </tbody>
+      </table>
+      <div className="bg-secondary-text-white text-center p-2">
+        <VisibilityControl 
+          description="Completed Tasks"
+          isChecked={showCompleted}
+          callback={checked=>setShowCompleted(checked)}
+        />
+      </div>
+
+      {
+        showCompleted&&(
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Description</th>
+                <th>Done</th>
+              </tr>
+            </thead>
+            <tbody>
+              {taskTableRows(true)}
+            </tbody>
+          </table>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
